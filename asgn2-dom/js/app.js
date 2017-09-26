5b916ee00e2744222f021cfa977b9b2b2f1a6244
shekodn <sdiazn19@gmail.com>
2017-09-26 17:44:00 +0800

    Creates a model to represent To-do List

--- asgn2-dom/js/app.js
+++ asgn2-dom/js/app.js
@@ -3,4 +3,20 @@
 
 	// Your starting point. Enjoy the ride!
 
+	// Create a model to represent To-do List
+	// An array of objects, with each object having two properties: "completed" (true/false)
+	// "description" (string);
+	// # of to-do items (that are not marked as completed)
+	function Item(){
+		this.isCompleted = false;
+		this.description = '';
+	}
+
+	function List(){
+		this.items = [];
+	}
+
+	
+
+
 })(window);
