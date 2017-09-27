fe3fb7e73ac987d27be9b89beb9e38998baf703a
shekodn <sdiazn19@gmail.com>
2017-09-28 01:10:12 +0800

    fixes parent parent bug

--- asgn2-dom/js/app.js
+++ asgn2-dom/js/app.js
@@ -27,7 +27,6 @@
 	var items = [];
 	var itemsLeft = 0;
 
-	$(function(){
 
 			$('#addItem').keypress(function (e) {
 				var key = e.which;
@@ -35,14 +34,14 @@
 				if(key == 13 && $('#addItem').val() != "") {// the enter key code
 
 					console.log('Adds item');
-					console.log(items);
 					var item = {isCompleted:false, description:"no desc"};
+					item.description = $('#addItem').val();
 					items.push(item);
 					$('.todo-count').html(itemsLeft);
-					item.description = $('#addItem').val();
 					$("#todo-list").append('<li><div class="view"> <input class="toggle" type="checkbox" > <label>' + item.description + '</label> <button class="destroy"> </button> </div> </li>');
 					$('input[name = butAssignProd]').click();
-
+					console.log('items has ' + items.length + ' elements');
+					console.log(items);
 					return false;
 				} else {
 					console.log('Cannot add item');
@@ -50,31 +49,39 @@
 			});
 
 	    $(document).on('click','.destroy',function(){
+					var label = $(this).index();
+					console.log('grand ' + $(this).parent().parent());
+					console.log('dad ' +  $(this).parent().parent());
+					$(this).parent().parent().remove();
 	        $(this).parent().remove();
+
+					if (label > -1) {
+						console.log('label ' + label);
+					    items.splice(label, 1);
+					}
+					console.log('items has ' + items.length + ' elements');
+					console.log(items);
+
 	    });
 
 			$(document).on('click','.toggle',function(){
+
 
 				if ($(this).closest('li').hasClass('completed')){
 
 					$(this).closest('li').removeClass('completed');
 					var label = $(this).index();
+					console.log('LABEL: ' + label);
 					items[label].isCompleted = false;
 					console.log('Task #' + label + ' is completed? = ' + items[label].isCompleted);
 
 				} else {
 					$(this).closest('li').addClass('completed');
 					var label = $(this).index();
+					console.log('LABEL: ' + label);
 					items[label].isCompleted = true;
 					console.log('Task #' + label + ' is completed? = ' + items[label].isCompleted);
-
 				}
-
 			});
-
-	});
-
-
-
 
 })(window);
