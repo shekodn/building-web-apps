7b369c8fa6d7ddfc88d60e677e409ccfab5671e3
shekodn <sdiazn19@gmail.com>
2017-09-28 12:12:57 +0800

    refactors the couns of items

--- asgn2-dom/js/app.js
+++ asgn2-dom/js/app.js
@@ -6,21 +6,19 @@
 	// Create a model to represent To-do List
 	// An array of objects, with each object having two properties: "completed" (true/false)
 	// "description" (string);
-	// # of to-do items (that are not marked as completed)
 	function Item(){
 		this.isCompleted = false;
 		this.description = '';
 	}
 
-	function List(){
-		this.items = [];
+	//Function to count <li> items in <ul>
+	function countsItems(number) {
+		if(number == 1 ){
+			$('.todo-count').html(number + ' item left');
+		} else{
+			$('.todo-count').html(number + ' items left');
+		}
 	}
-
-	function itemsLeft(){
-		$('.todo-count').html(iItems);
-
-	}
-
 
 	//Adds to list function
 	var items = [];
@@ -31,127 +29,113 @@
 	var itemsNotCompleted = 0;
 
 
-			$('#addItem').keypress(function (e) {
-				var key = e.which;
+	$('#addItem').keypress(function (e) {
+		var key = e.which;
 
-				if(key == 13 && $('#addItem').val() != "") {// the enter key code
+		if(key == 13 && $('#addItem').val() != "") {// the enter key code
 
-					console.log('Adds item');
-					var item = {isCompleted:false, description:"no desc"};
-					item.description = $('#addItem').val();
-					items.push(item);
-					$("#todo-list").append('<li><div class="view"> <input class="toggle" type="checkbox" > <label>' + item.description + '</label> <button class="destroy"> </button> </div> </li>');
-					$('input[name = butAssignProd]').click();
-					console.log('items has ' + items.length + ' elements');
-					console.log(items);
+			console.log('Adds item');
+			var item = {isCompleted:false, description:"no desc"};
+			item.description = $('#addItem').val();
+			items.push(item);
+			$("#todo-list").append('<li><div class="view"> <input class="toggle" type="checkbox" > <label>' + item.description + '</label> <button class="destroy"> </button> </div> </li>');
+			$('input[name = butAssignProd]').click();
+			console.log('items has ' + items.length + ' elements');
+			console.log(items);
 
-					//adds items
-					iItems += 1;
-					if(iItems == 1){
-						$('.todo-count').html(iItems + ' item');
+			//adds items
+			iItems += 1;
+			countsItems(iItems);
+			return false;
 
-					} else{
-						$('.todo-count').html(iItems + ' items');
+		} else {
+			console.log('Cannot add item');
+		}
+	});
 
-					}
+  $(document).on('click','.destroy',function(){
+			var label = $(this).index();
 
-					return false;
+			if (label > -1) {
+				console.log('label ' + label);
+			    items.splice(label, 1);
+			}
 
-				} else {
-					console.log('Cannot add item');
-				}
-			});
+			$(this).parent().parent().remove();
+			$(this).parent().remove();
+			$(this).closest().remove();
 
-	    $(document).on('click','.destroy',function(){
-					var label = $(this).index();
-
-					if (label > -1) {
-						console.log('label ' + label);
-					    items.splice(label, 1);
-					}
-
-					$(this).parent().parent().remove();
-					$(this).parent().remove();
-					$(this).closest().remove();
-
-					//removes items
-					iItems -= 1;
-
-					if(iItems == 1){
-						$('.todo-count').html(iItems + ' item');
-
-					} else{
-						$('.todo-count').html(iItems + ' items');
-					}
-
-					console.log('items has ' + items.length + ' elements');
-					console.log(items);
-
-	    });
-
-			$(document).on('click','.toggle',function(){
+			//removes items
+			iItems -= 1;
+			countsItems(iItems);
 
 
-				if ($(this).closest('li').hasClass('completed')){
+			console.log('items has ' + items.length + ' elements');
+			console.log(items);
 
-					$(this).closest('li').removeClass('completed');
-					var label = $(this).index();
-					console.log('LABEL: ' + label);
-					items[label].isCompleted = false;
-					console.log('Task #' + label + ' is completed? = ' + items[label].isCompleted);
+  });
 
-				} else {
-					$(this).closest('li').addClass('completed');
-					var label = $(this).index();
-					console.log('LABEL: ' + label + ' inner text ' + label.innerText);
-					items[label].isCompleted = true;
-					console.log('Task #' + label + ' is completed? = ' + items[label].isCompleted);
-				}
-
-				var remainingItems = iItems - $("#todo-list .completed").length;
-
-				if(remainingItems ==  1){
-
-					$('.todo-count').html(remainingItems + ' item left');
-
-				} else {
-
-					$('.todo-count').html(remainingItems + ' items left');
-
-				}
+	$(document).on('click','.toggle',function(){
 
 
-			});
+		if ($(this).closest('li').hasClass('completed')){
 
-			//filters
-			$("#filters li").click(function() {
+			$(this).closest('li').removeClass('completed');
+			var label = $(this).index();
+			console.log('LABEL: ' + label);
+			items[label].isCompleted = false;
+			console.log('Task #' + label + ' is completed? = ' + items[label].isCompleted);
 
-				var index = $("#filters li").index(this);
-				// console.log("Index " + index + " was clicked");
+		} else {
+			$(this).closest('li').addClass('completed');
+			var label = $(this).index();
+			console.log('LABEL: ' + label + ' inner text ' + label.innerText);
+			items[label].isCompleted = true;
+			console.log('Task #' + label + ' is completed? = ' + items[label].isCompleted);
+		}
 
-				//All
-				if(index == 0){
-					$('.todo-count').html(iItems);
+		var remainingItems = iItems - $("#todo-list .completed").length;
 
-				} else if (index == 1) { //Active
-					$('.todo-count').html(iItems - $("#todo-list .completed").length);
+		if(remainingItems ==  1){
 
-				} else { //completed
-					$('.todo-count').html($("#todo-list .completed").length);
-				}
+			$('.todo-count').html(remainingItems + ' item left');
 
-			});
+		} else {
+
+			$('.todo-count').html(remainingItems + ' items left');
+
+		}
 
 
-			$( document ).ready(function() {
-				if(iItems == 1){
-					$('.todo-count').html(iItems + ' item');
+	});
 
-				} else{
-					$('.todo-count').html(iItems + ' items');
+	// Filters
+	// this function counts the number of total items, completed items and active
+	// items and updates the number dinamically
+	$("#filters li").click(function() {
 
-				}
-			});
+		var index = $("#filters li").index(this);
+		// console.log("Index " + index + " was clicked");
+
+		//All
+		if(index == 0){
+			$('.todo-count').html(iItems  + ' items');
+
+		} else if (index == 1) { //Active
+			$('.todo-count').html(iItems - $("#todo-list .completed").length  + ' items left');
+
+		} else { //completed
+			$('.todo-count').html($("#todo-list .completed").length + ' items completed');
+		}
+
+	});
+
+
+	$( document ).ready(function() {
+
+		countsItems(iItems);
+
+	});
 
 
 
