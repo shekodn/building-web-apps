a0ddf5e4f29a07028713b639f8a46a4eb96b7557
shekodn <sdiazn19@gmail.com>
2017-10-27 02:31:29 +0800

    replaces succesfully

--- asgn3/js/p2.js
+++ asgn3/js/p2.js
@@ -1,4 +1,5 @@
 // You need to "require" persons.js in this file to access the array of persons
+var persons = require('./persons');
 
 module.exports = function(req, res) {
 
@@ -8,10 +9,17 @@ module.exports = function(req, res) {
   var Q = req;
 
   // Let P be the array of persons
+  var P = persons;
 
   // Replace P[i] by Q if P[i].index == Q.index
+  console.log("Before: ");
+  console.log(P[Q.index]);
+  P[Q.index] = Q;
+  console.log(" After: ");
+  console.log(P[Q.index]);
 
   // Send 1 in the body of the response if a replacement successfully
   // took place.
   // Otherwise, send 0 in the body of the response.
+
 }
