c6a91e9f05ca8ec6d0b496d3fa4e95501637784a
shekodn <sdiazn19@gmail.com>
2017-10-26 20:46:09 +0800

    adds only if contains prefix

--- asgn3/index.js
+++ asgn3/index.js
@@ -10,17 +10,19 @@ var p2 = require('./js/p2.js');
 //Contains Q (array of persons)
 var persons = p1();
 
+// Add routing rules in this file so that
+// (1) Any GET request sent to "/services/p1"
+// will be handled by the function exported by p1.js.
 app.get('/services/p1', function(req, res){
   res.send(persons); //replace with your data here
 });
 
 
-// Add routing rules in this file so that
-// (1) Any GET request sent to "/services/p1"
-// will be handled by the function exported by p1.js.
 // (2) Any POST request sent to "/services/p2"
 // will be handled by the function exported by p2.js.
-//
+
+
+
 // For POST requests, you may need to use a body-parser
 // to decode its body here first.
 
