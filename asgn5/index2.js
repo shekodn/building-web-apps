629c90b797a35b9d826d3b275f7a3160e500dcc2
shekodn <sdiazn19@gmail.com>
2017-11-30 23:04:10 +0800

    finishes assgn5

--- asgn5/index2.js
+++ asgn5/index2.js
@@ -4,6 +4,13 @@ let cookieParser = require('cookie-parser');
 
 app.use(cookieParser());
 
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:8081");
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
 app.get('/simpleget', function(req, res) {
   let foo = req.cookies['foo'];
   if (foo)
